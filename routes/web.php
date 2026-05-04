9642c1897cbfcfa36e5428a7d9434e0505744e30
eskiel <eskieltroymanalu@gmail.com>
2026-05-04 08:36:47 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -99,6 +99,11 @@ Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
+Route::get('/forgot-password', [AuthController::class, 'showForgotPassword'])->name('password.request');
+Route::post('/forgot-password', [AuthController::class, 'sendResetLink'])->name('password.email');
+Route::get('/reset-password/{token}', [AuthController::class, 'showResetForm'])->name('password.reset');
+Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.update');
+
 // ==================== ADMIN ROUTES ====================
 Route::prefix('admin')->middleware('auth')->group(function () {
     Route::get('/', function () {
