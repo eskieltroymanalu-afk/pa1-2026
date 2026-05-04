c24dd07a5fa7df64eb3415cd1c43fcde7e2daee1
eskiel <eskieltroymanalu@gmail.com>
2026-05-04 14:21:07 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,4 +37,14 @@ class HomeController extends Controller
 
         return view('pages.home', compact('galeri', 'berita', 'destinasi', 'banners'));
     }
+
+    public function umkm()
+    {
+        return view('pages.umkm');
+    }
+
+    public function budaya()
+    {
+        return view('pages.budaya');
+    }
 }
